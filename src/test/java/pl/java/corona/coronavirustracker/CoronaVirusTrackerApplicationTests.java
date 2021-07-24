aafff57d031bc8d05a6bebb81b691ce2686ffe0b
Przemek Nosek <przemek.nosek@o2.pl>
2021-07-24 11:02:25 +0200

    commit

---
+++ src/test/java/pl/java/corona/coronavirustracker/CoronaVirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.java.corona.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
