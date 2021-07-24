ffcfdbe453519328aede31ee986bab6416b1ca7e
Przemek Nosek <przemek.nosek@o2.pl>
2021-07-24 11:05:20 +0200

    test

--- src/main/java/pl/java/corona/coronavirustracker/CoronaVirusTrackerApplication.java
+++ src/main/java/pl/java/corona/coronavirustracker/CoronaVirusTrackerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoronaVirusTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoronaVirusTrackerApplication.class, args);
+        SpringApplication.run(CoronaVirusTrackerApplication.class, args); //fsafsafa
     }
 
 }
